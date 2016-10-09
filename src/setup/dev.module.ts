fdc952f53c871d7a7db6610c4bbb9c9b87f5e05b
Brecht Billiet <billietbrecht@gmail.com>
2016-10-09 12:02:59 +0200

    integration with ngrx store

--- src/setup/dev.module.ts
+++ src/setup/dev.module.ts
@@ -1,16 +1,27 @@
 import {AppModule} from "../app";
 import {NgModule, Component} from "@angular/core";
+import {StoreModule} from "@ngrx/store";
+import {rootReducer} from "../statemanagement/rootReducer";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {StoreLogMonitorModule, useLogMonitor} from "@ngrx/store-log-monitor";
 @Component({
     selector: "application-wrapper",
     template: `   
         <application></application>
+        <ngrx-store-log-monitor toggleCommand="ctrl-t" positionCommand="ctrl-m"></ngrx-store-log-monitor>
 `
 })
 export class ApplicationWrapperContainer {
 }
 
 @NgModule({
-    imports: [AppModule],
+    imports: [
+        StoreModule.provideStore(rootReducer), StoreDevtoolsModule.instrumentStore({
+            monitor: useLogMonitor({
+                visible: false,
+                position: "right"
+            })
+        }), StoreLogMonitorModule, AppModule],
     declarations: [ApplicationWrapperContainer],
     bootstrap: [ApplicationWrapperContainer]
 })
--- src/setup/prod.module.ts
+++ src/setup/prod.module.ts
@@ -1,5 +1,7 @@
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA, Component} from "@angular/core";
 import {AppModule} from "../app";
+import {StoreModule} from "@ngrx/store";
+import {rootReducer} from "../statemanagement/rootReducer";
 
 @Component({
     selector: "application-wrapper",
@@ -11,7 +13,7 @@ export class ApplicationWrapperContainer {
 }
 
 @NgModule({
-    imports: [AppModule],
+    imports: [StoreModule.provideStore(rootReducer), AppModule],
     declarations: [ApplicationWrapperContainer],
     bootstrap: [ApplicationWrapperContainer],
     schemas: [CUSTOM_ELEMENTS_SCHEMA,]
