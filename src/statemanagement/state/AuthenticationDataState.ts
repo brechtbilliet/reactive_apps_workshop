97cb0af1b42012ded67179ecdfae785e3792b76b
Brecht Billiet <billietbrecht@gmail.com>
2016-10-09 11:48:37 +0200

    removed statemanagement

--- src/statemanagement/state/AuthenticationDataState.ts
+++
@@ -1,6 +0,0 @@
-import {Account} from "../../authentication/types/Account";
-export interface AuthenticationDataState {
-    isAuthenticated: boolean;
-    jwtToken: string;
-    account: Account;
-}
