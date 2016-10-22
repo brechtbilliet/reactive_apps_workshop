bb31488cdb41e22ebe13dc7df47bf5759dfbd5c3
Brecht Billiet <billietbrecht@gmail.com>
2016-10-22 07:53:13 +0200

    implementing stock page

--- src/stock/containers/stock-page/stock-page.container.ts
+++ src/stock/containers/stock-page/stock-page.container.ts
@@ -6,13 +6,14 @@ import {Subscription, Observable} from "rxjs";
 import {Store} from "@ngrx/store";
 import {ApplicationState} from "../../../statemanagement/state/ApplicationState";
 import * as orderBy from "lodash/orderBy";
+import * as sumBy from "lodash/sumBy";
 import {removeWine, updateRateWine, updateStockWine} from "../../../statemanagement/actionCreators";
 @Component({
     selector: "stock-page",
     template: `
         <default-page>
             <collapsable-sidebar class="hidden-sm hidden-xs">
-                <favorite-wines (setStock)="onSetStock($event)" [wines]="favoriteWines">
+                <favorite-wines (setStock)="onSetStock($event)" [wines]="favoriteWines$|async">
                 </favorite-wines>
             </collapsable-sidebar>
             <main>
@@ -33,8 +34,8 @@ import {removeWine, updateRateWine, updateStockWine} from "../../../statemanagem
                     <div class="col-sm-12">
                         <h2>
                             <i class="fa fa-user"></i>&nbsp;My wines
-                            <span class="badge badge-primary">todo</span>
-                            <span class="badge badge-primary">todo euro</span>
+                            <span class="badge badge-primary">{{numberOfWines$|async}}</span>
+                            <span class="badge badge-primary">{{worth$|async}} euro</span>
                         </h2>
                     </div>
                 </div>
@@ -55,6 +56,11 @@ export class StockPageContainer implements OnDestroy {
     searchCtrl = new FormControl("");
     term$ = this.searchCtrl.valueChanges.startWith("");
     wines$ = this.store.select(state => state.data.wines);
+    numberOfWines$ = this.wines$.map(wines => sumBy(wines, (wine: Wine) => wine.inStock));
+    worth$ = this.wines$.map(wines => sumBy(wines, (wine: Wine) => wine.price * wine.inStock).toFixed(2));
+    favoriteWines$ = this.wines$.map((wines: Wine[]) => wines.filter((wine: Wine) => wine.myRating > 3))
+        .map(wines => orderBy(wines, ["myRating"], ["desc"]).slice(0,5));
+
     matchingWines$ = Observable.combineLatest(this.term$, this.wines$,
         (term: string, wines: Array<Wine>) => {
             return wines.filter(wine => wine.name.toLowerCase().indexOf(term) > -1);
