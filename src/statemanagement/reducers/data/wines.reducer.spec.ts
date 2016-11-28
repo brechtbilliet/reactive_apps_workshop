2fc9de2f63446969b1f5908318a6d06e92f8e317
kwintenp <kwinten.pisman@gmail.com>
2016-11-28 17:25:08 +0100

    Changed the state management layer to work with type safe actions through classes.

--- src/statemanagement/reducers/data/wines.reducer.spec.ts
+++ src/statemanagement/reducers/data/wines.reducer.spec.ts
@@ -1,13 +1,7 @@
 import {Wine} from "../../../stock/entities/Wine";
 import {winesReducer} from "./wines.reducer";
-import {
-    DATA_WINES_ADD,
-    DATA_WINES_ADD_ALL,
-    DATA_WINES_REMOVE,
-    DATA_WINES_UPDATE,
-    DATA_WINES_UPDATE_RATE,
-    DATA_WINES_UPDATE_STOCK
-} from "../../actionTypes";
+import * as _ from "lodash";
+import {AddWine, SetAllWines, RemoveWine, UpdateWine, UpdateRate, UpdateStock} from "../../actions/data/wine";
 let deepfreeze = require("deep-freeze");
 
 describe("reducer > data", () => {
@@ -15,22 +9,22 @@ describe("reducer > data", () => {
         it("should return a new instance with the correct state", () => {
             let initialState: Array<Wine> = [];
             initialState.push(new Wine("initiaial", "wine"));
-            let payload = {wine: new Wine()};
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {type: DATA_WINES_ADD, payload});
+            let wine: Wine = new Wine();
+            let changedState: Array<Wine> = winesReducer(initialState, new AddWine(wine));
             expect(changedState).not.toBe(initialState);
             expect(changedState.length).toBe(2);
-            expect(changedState[1]).toBe(payload.wine);
+            expect(changedState[1]).toBe(wine);
         });
     });
-    describe("case DATA_WINES_ADD_ALL", () => {
+    describe("case DATA_WINES_SET_ALL", () => {
         it("should return a new instance with the correct state", () => {
             let initialState: Array<Wine> = [];
-            let payload = {wines: [new Wine(), new Wine(), new Wine()]};
+            let wines: Wine[] = [new Wine(), new Wine(), new Wine()];
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {type: DATA_WINES_ADD_ALL, payload});
+            let changedState: Array<Wine> = winesReducer(initialState, new SetAllWines(wines));
             expect(changedState).not.toBe(initialState);
-            payload.wines.forEach((wine: Wine, index: number) => {
+            _.each(wines, (wine: Wine, index: number) => {
                 expect(wine).toBe(changedState[index]);
             });
         });
@@ -42,13 +36,11 @@ describe("reducer > data", () => {
             initialState[1]._id = "fakeid2";
             initialState[2]._id = "fakeid3";
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {
-                type: DATA_WINES_REMOVE,
-                payload: {_id: initialState[0]._id}
-            });
+            let changedState: Array<Wine> = winesReducer(initialState,
+                new RemoveWine("fakeid1"));
             expect(changedState).not.toBe(initialState);
             expect(changedState.length).toBe(2);
-            expect(changedState.filter((wine: Wine) => wine._id === initialState[0]._id).length).toBe(0);
+            expect(_.filter(changedState, (wine: Wine) => wine._id === initialState[0]._id).length).toBe(0);
         });
     });
     describe("case DATA_WINES_UPDATE", () => {
@@ -61,10 +53,9 @@ describe("reducer > data", () => {
                 name: "updated"
             });
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {
-                type: DATA_WINES_UPDATE,
-                payload: {_id: initialState[0]._id, wine: updateWine}
-            });
+            let changedState: Array<Wine> = winesReducer(initialState,
+                new UpdateWine("fakeid1", updateWine)
+            );
             expect(changedState).not.toBe(initialState);
             expect(changedState[0]).not.toBe(initialState[0]);
             expect(changedState[0]).not.toBe(updateWine);
@@ -79,10 +70,9 @@ describe("reducer > data", () => {
             initialState[2]._id = "fakeid3";
             let newRating: number = 5;
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {
-                type: DATA_WINES_UPDATE_RATE,
-                payload: {_id: initialState[0]._id, myRating: newRating}
-            });
+            let changedState: Array<Wine> = winesReducer(initialState,
+                new UpdateRate("fakeid1", newRating)
+            );
             expect(changedState).not.toBe(initialState);
             expect(changedState[0]).not.toBe(initialState[0]);
             expect(changedState[0].myRating).toBe(newRating);
@@ -96,10 +86,9 @@ describe("reducer > data", () => {
             initialState[2]._id = "fakeid3";
             let newInStock: number = 5;
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {
-                type: DATA_WINES_UPDATE_STOCK,
-                payload: {_id: initialState[0]._id, inStock: newInStock}
-            });
+            let changedState: Array<Wine> = winesReducer(initialState,
+                new UpdateStock("fakeid1", newInStock)
+            );
             expect(changedState).not.toBe(initialState);
             expect(changedState[0]).not.toBe(initialState[0]);
             expect(changedState[0].inStock).toBe(newInStock);
@@ -109,7 +98,7 @@ describe("reducer > data", () => {
         it("should return the same state", () => {
             let initialState: Array<Wine> = [new Wine(), new Wine(), new Wine()];
             deepfreeze(initialState);
-            let changedState: Array<Wine> = winesReducer(initialState, {type: null});
+            let changedState: Array<Wine> = winesReducer(initialState, {type: null} as any);
             expect(changedState).toBe(initialState);
         });
     });
