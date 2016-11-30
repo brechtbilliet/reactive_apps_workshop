69029edad1d98adca9b5c56c087dd6a8c657f5f1
kwintenp <kwinten.pisman@gmail.com>
2016-11-30 19:50:44 +0100

    Changed the state management layer to work with type safe actions through classes.

--- src/statemanagement/reducers/containers/collapsable-sidebar.reducer.spec.ts
+++ src/statemanagement/reducers/containers/collapsable-sidebar.reducer.spec.ts
@@ -1,41 +1,43 @@
+import {CollapsableSidebarContainerState} from "../../state/ContainersState";
 import {collapsableSidebarReducer} from "./collapsable-sidebar.reducer";
-import {CONTAINER_COLLAPSABLESIDEBAR_TOGGLE} from "../../actionTypes";
+import {ToggleSidebar} from "../../actions/containers/sidebar";
 import {Dispatcher} from "@ngrx/store";
 let deepfreeze = require("deep-freeze");
 
 describe("reducer: containers > collaspableSidebarReducer", () => {
     describe("case CONTAINER_COLLAPSABLESIDEBAR_TOGGLE", () => {
         it("should return a new state with a different isCollapsed value", () => {
-            let initialState: {isCollapsed: boolean} = {
+            let initialState: CollapsableSidebarContainerState = {
                 isCollapsed: false
             };
             deepfreeze(initialState);
-            let changedState: {isCollapsed: boolean} =
-                collapsableSidebarReducer(initialState, {type: CONTAINER_COLLAPSABLESIDEBAR_TOGGLE});
+            let changedState: CollapsableSidebarContainerState =
+                collapsableSidebarReducer(initialState, new ToggleSidebar());
             expect(changedState).not.toBe(initialState);
             expect(changedState.isCollapsed).toBe(true);
             initialState = {
                 isCollapsed: true
             };
-            changedState = collapsableSidebarReducer(initialState, {type: CONTAINER_COLLAPSABLESIDEBAR_TOGGLE});
+            changedState = collapsableSidebarReducer(initialState, new ToggleSidebar());
             expect(changedState.isCollapsed).toBe(false);
 
         });
     });
     describe("case default", () => {
         it("should return the exact same reference as before", () => {
-            let initialState: {isCollapsed: boolean} = {
+            let initialState: CollapsableSidebarContainerState = {
                 isCollapsed: false
             };
             deepfreeze(initialState);
-            let changedState: {isCollapsed: boolean} = collapsableSidebarReducer(initialState, {type: null});
+            let changedState: CollapsableSidebarContainerState = collapsableSidebarReducer(initialState, {type: null}) as any;
             expect(changedState).toBe(initialState);
         });
     });
     describe("case @ngrx/store/init", () => {
         it("should return the default value for the state param", () => {
-            let changedState: {isCollapsed: boolean} = collapsableSidebarReducer(undefined, {type: Dispatcher.INIT});
+            let changedState: CollapsableSidebarContainerState = collapsableSidebarReducer(undefined, {type: Dispatcher.INIT} as any);
             expect(changedState.isCollapsed).toBeFalsy();
         });
     });
-});
+})
+;
