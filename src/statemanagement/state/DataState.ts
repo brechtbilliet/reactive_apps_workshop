287ae86197069309cf3aac82a4d33efc3801fff4
kwintenp <kwinten.pisman@gmail.com>
2016-11-30 19:30:08 +0100

    Changed the state management layer to work with type safe actions through classes.

--- src/statemanagement/state/DataState.ts
+++ src/statemanagement/state/DataState.ts
@@ -1,4 +1,11 @@
+import {Wine} from "../../stock/entities/Wine";
 import {Account} from "../../authentication/types/Account";
+
+export interface DataState {
+    authentication: AuthenticationDataState;
+    wines: Array<Wine>;
+}
+
 export interface AuthenticationDataState {
     isAuthenticated: boolean;
     jwtToken: string;
