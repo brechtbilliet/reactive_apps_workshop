ab491c536a2e63909bfd33934642d1a802f6a96b
KwintenP <kwinten.pisman@gmail.com>
2017-02-01 19:47:06 +0100

    Reverted erronous checkin.

--- src/statemanagement/rootReducer.ts
+++ src/statemanagement/rootReducer.ts
@@ -2,8 +2,7 @@ import {collapsableSidebarReducer} from "./reducers/containers/collapsable-sideb
 import {authenticationReducer} from "./reducers/data/authentication.reducer";
 import {winesReducer} from "./reducers/data/wines.reducer";
 import {applicationReducer} from "./reducers/containers/application.reducer";
-import {combineReducers, Action} from "@ngrx/store";
-import {ApplicationState} from "./state/ApplicationState";
+import {combineReducers} from "@ngrx/store";
 let dataReducers = combineReducers({
     authentication: authenticationReducer,
     wines: winesReducer
@@ -17,16 +16,6 @@ let productionReducer = combineReducers({
     containers: containersReducers
 });
 
-export const RESET_STORE = "RESET_STORE";
-
-const createResetReducer = (reducer: any) => {
-    console.log("called");
-    return (state: ApplicationState, action: Action) => {
-        console.log(state, action);
-        return /*action.payload === RESET_STORE ? {} :*/ productionReducer(state, action)
-    }
-}
-
-export function rootReducer(state: ApplicationState, action: Action) {
-    return createResetReducer(productionReducer(state, action));
+export function rootReducer(state: any, action: any) {
+    return productionReducer(state, action);
 }
