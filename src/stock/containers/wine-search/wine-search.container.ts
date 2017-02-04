c35aaf03cb35ec8693cbbf38e2ba4bb095a7841e
Brecht Billiet <billietbrecht@gmail.com>
2017-02-04 15:16:49 +0100

    better reactive flow

--- src/stock/containers/wine-search/wine-search.container.ts
+++ src/stock/containers/wine-search/wine-search.container.ts
@@ -1,8 +1,7 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnDestroy, OnInit} from "@angular/core";
-import {Subscription} from "rxjs/Subscription";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
-import {Product, WineComSearchResult, WineComService} from "../../services/wineCom.service";
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges} from "@angular/core";
+import { Product, WineComSearchResult, WineComService } from "../../services/wineCom.service";
 import {FormControl} from "@angular/forms";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 @Component({
     selector: "wine-search",
@@ -13,7 +12,7 @@ import {FormControl} from "@angular/forms";
                 Name (*)
             </label>
             <div class="col-sm-8">
-                <input type="text" [formControl]="control" class="form-control input-lg" id="searchInput" 
+                <input type="text" class="form-control input-lg" id="searchInput" [formControl]="control"
                     autocomplete="off" placeholder="Name"/>
                 <span *ngIf="control.valid" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
                 <ul class="wine-search-results">
@@ -25,43 +24,34 @@ import {FormControl} from "@angular/forms";
         </div>
     `
 })
-export class WineSearchContainer implements OnDestroy, OnInit {
-    @Input() control: FormControl;
+export class WineSearchContainer implements OnChanges {
+    @Input() name: string;
     @Output() selectWine = new EventEmitter<Product>();
+    control = new FormControl("");
 
-    winesToShow$ = new BehaviorSubject([]);
+    private showResults$ = new BehaviorSubject(true);
 
-    private foundWineName: string;
-    private subscriptions: Array<Subscription> = [];
+    private clear$ = this.showResults$.filter(val => !val)
+        .map(() => []);
+
+    private winesToShow$ = this.control.valueChanges
+        .do((value: string) => this.showResults$.next(false)) // user types, hide the results
+        .filter(value => value.length > 2)
+        .debounceTime(300)
+        .switchMap(value => this.wineComService.search(value))
+        .map((res: WineComSearchResult) => res.products.list)
+        .merge(this.clear$)
+        .distinctUntilChanged();
 
     constructor(private wineComService: WineComService) {
     }
 
+    ngOnChanges(): void {
+        this.control.setValue(this.name, {emitEvent: false}); // don't call valuechanges again
+    }
+
     onSelectWine(wine: Product): void {
-        this.foundWineName = wine.name;
         this.selectWine.emit(wine);
-        this.winesToShow$.next([]); // clear
-    }
-
-    ngOnDestroy(): void {
-        this.subscriptions.forEach(sub => sub.unsubscribe());
-    }
-
-    ngOnInit(): void {
-        let subscription = this.control.valueChanges
-            .do((value: string) => {
-                if (value.length < 3) {
-                    this.winesToShow$.next([]); // clear
-                }
-            })
-            .debounceTime(300)
-            .distinctUntilChanged()
-            .filter(value => value.length > 2 && value !== this.foundWineName)
-            .switchMap(value => this.wineComService.search(value))
-            .map((res: WineComSearchResult) => res.products.list)
-            .subscribe((wines: Array<Product>) => {
-                this.winesToShow$.next(wines);
-            });
-        this.subscriptions.push(subscription);
+        this.showResults$.next(false);
     }
 }
